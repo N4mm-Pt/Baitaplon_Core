272c99abd556e6c7bbd8d798352b36e856eb3610
kentw <kentw@DESKTOP-HV73J9G>
2022-11-28 09:07:14 +0700

    Add project files.

---
+++ Baitaplon_Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Baitaplon_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
